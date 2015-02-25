c786c03229eeb5d1107f1966c3db146fbfd22445
czinsii <zinsx040@umn.edu>
2015-02-24 22:52:51 -0600

    Implemented eraser

--- src/tools/ToolEraser.cpp
+++ src/tools/ToolEraser.cpp
@@ -16,6 +16,16 @@
  * 		  color The currently selected color
  */
 void ToolEraser::applyMask( int x, int y, PixelBuffer &buffer, ColorData color ) {
+	ColorData bgColor = buffer.getBackgroundColor();
+	int originX = x - ( m_maskWidth / 2 );
+	int originY = ( buffer.getHeight() - y ) - ( m_maskHeight / 2 );
+
+	for( int row = 0; row < m_maskHeight; row++ ) {
+		for( int col = 0; col < m_maskWidth; col++ ) {
+			float opacity = m_mask[row * m_maskWidth + col];
+			buffer.setPixel( col + originX, row + originY, ( bgColor * opacity ) + ( buffer.getPixel( col + originX, row + originY ) * ( 1.0f - opacity ) ) );
+		}
+	}
 
 }
 
